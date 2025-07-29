eec7504f22d646a291c448c6493628fa90207bd9
AyushSingh360 <ayushsingh21109@gmail.com>
2025-07-29 15:28:36 +0000

    [LeetCode Sync] - Two Sum - Runtime - 0 ms (100.00%), Memory - 13 MB (82.48%)

--- leetcode/0001-two-sum/solution.py
+++ leetcode/0001-two-sum/solution.py
@@ -1,10 +1,9 @@
-class Solution:
+class Solution(object):
     def twoSum(self, nums, target):
-        num_map = {}  # Dictionary to store num: index
-
+        hashmap = {}
         for i, num in enumerate(nums):
-            complement = target - num
-            if complement in num_map:
-                return [num_map[complement], i]
-            num_map[num] = i
+            diff = target - num
+            if diff in hashmap:
+                return [hashmap[diff], i]
+            hashmap[num] = i
 
