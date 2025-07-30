584bc5d08d5391d3e91887ee62566f66d6e1e6d8
AyushSingh360 <ayushsingh21109@gmail.com>
2025-07-30 16:57:08 +0000

    [LeetCode Sync] - Find First and Last Position of Element in Sorted Array - Runtime - 0 ms (100.00%), Memory - 13.2 MB (38.59%)

---
+++ leetcode/0034-find-first-and-last-position-of-element-in-sorted-array/solution.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def searchRange(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        def findBound(isFirst):
+            left, right = 0, len(nums) - 1
+            result = -1
+            while left <= right:
+                mid = (left + right) // 2
+                if nums[mid] == target:
+                    result = mid
+                    if isFirst:
+                        right = mid - 1  # Move left
+                    else:
+                        left = mid + 1   # Move right
+                elif nums[mid] < target:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+            return result
+
+        first = findBound(True)
+        last = findBound(False)
+        return [first, last]
+
