fc70233a2acea414cefd75187a56e859c29b08d4
AyushSingh360 <ayushsingh21109@gmail.com>
2025-08-02 10:14:20 +0000

    [LeetCode Sync] - Merge Intervals - Runtime - 7 ms (97.25%), Memory - 16 MB (53.04%)

---
+++ leetcode/0056-merge-intervals/solution.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def merge(self, intervals):
+        """
+        :type intervals: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        if not intervals:
+            return []
+
+        # Step 1: Sort intervals based on the start time
+        intervals.sort(key=lambda x: x[0])
+        merged = [intervals[0]]
+
+        for current in intervals[1:]:
+            prev = merged[-1]
+
+            # Overlapping intervals -> merge
+            if current[0] <= prev[1]:
+                prev[1] = max(prev[1], current[1])
+            else:
+                merged.append(current)
+
+        return merged
+
