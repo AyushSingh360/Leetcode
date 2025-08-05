d9b49192bb43c13215b60891f9fa65c25ea75880
AyushSingh360 <ayushsingh21109@gmail.com>
2025-08-05 02:20:01 +0000

    [LeetCode Sync] - Alternating Groups II - Runtime - 853 ms (16.28%), Memory - 20.8 MB (79.07%)

---
+++ leetcode/3483-alternating-groups-ii/solution.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def numberOfAlternatingGroups(self, colors, k):
+        """
+        :type colors: List[int]
+        :type k: int
+        :rtype: int
+        """
+        n = len(colors)
+        ans = cnt = 0
+        
+        # Traverse through 2n entries using modulo to simulate circular behavior
+        for i in range(n * 2):
+            if i > 0 and colors[i % n] == colors[(i - 1) % n]:
+                cnt = 1
+            else:
+                cnt += 1
+            
+            # Only count when we've seen at least k and are in the second round
+            if i >= n and cnt >= k:
+                ans += 1
+
+        return ans
+
