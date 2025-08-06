3b5bc0e5f5203af4cbe7ed4efe4a63c1cd8ce2e7
AyushSingh360 <ayushsingh21109@gmail.com>
2025-08-06 15:04:11 +0000

    [LeetCode Sync] - Count of Substrings Containing Every Vowel and K Consonants II - Runtime - 6635 ms (5.00%), Memory - 20.2 MB (25.00%)

---
+++ leetcode/3569-count-of-substrings-containing-every-vowel-and-k-consonants-ii/solution.py
@@ -0,0 +1,34 @@
+class Solution(object):
+    def countOfSubstrings(self, word, k):
+        VOWELS = set('aeiou')
+
+        def atMost(K):
+            if K < 0:
+                return 0
+            l = 0
+            res = 0
+            vowels = 0
+            unique_vowels = 0
+            last = {}
+            for r in range(len(word)):
+                c = word[r]
+                if c in VOWELS:
+                    vowels += 1
+                    if last.get(c, -1) < l:
+                        unique_vowels += 1
+                    last[c] = r
+
+                while (r - l + 1) - vowels > K:
+                    d = word[l]
+                    if d in VOWELS:
+                        vowels -= 1
+                        if last.get(d, -1) == l:
+                            unique_vowels -= 1
+                    l += 1
+
+                if unique_vowels == 5:
+                    res += min(last[v] for v in VOWELS) - l + 1
+            return res
+
+        return atMost(k) - atMost(k - 1)
+
