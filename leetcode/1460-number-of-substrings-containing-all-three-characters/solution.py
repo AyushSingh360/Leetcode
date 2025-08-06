5c6e402d7bbd6a61ca9d8098d14be226bd029c27
AyushSingh360 <ayushsingh21109@gmail.com>
2025-08-06 15:13:38 +0000

    [LeetCode Sync] - Number of Substrings Containing All Three Characters - Runtime - 145 ms (55.71%), Memory - 12.6 MB (74.64%)

---
+++ leetcode/1460-number-of-substrings-containing-all-three-characters/solution.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def numberOfSubstrings(self, s):
+        last_seen = {'a': -1, 'b': -1, 'c': -1}
+        result = 0
+
+        for i, char in enumerate(s):
+            last_seen[char] = i
+            if -1 not in last_seen.values():
+                # The smallest index of the three
+                min_index = min(last_seen.values())
+                result += min_index + 1
+        return result
+
