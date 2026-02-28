7d9e76f67beb460e9c2aae734cdcdb0d9ad6702f
AyushSingh360 <ayushsingh21109@gmail.com>
2026-02-28 14:36:37 +0000

    [LeetCode Sync] - Concatenation of Consecutive Binary Numbers - Runtime - 514 ms (79.09%), Memory - 15.6 MB (29.80%)

---
+++ leetcode/1800-concatenation-of-consecutive-binary-numbers/solution.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def concatenatedBinary(self, n):
+        MOD = 10**9 + 7
+        ans = 0
+        bits = 0
+        
+        for i in range(1, n + 1):
+            # If i is a power of 2, increase bit-length
+            if (i & (i - 1)) == 0:
+                bits += 1
+            
+            ans = ((ans << bits) + i) % MOD
+        
+        return ans
+
