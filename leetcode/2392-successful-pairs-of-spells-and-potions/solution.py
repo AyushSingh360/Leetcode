c2e4eb43e9b47b34874e871f6384e56a10aef0ef
AyushSingh360 <ayushsingh21109@gmail.com>
2026-03-04 02:46:55 +0000

    [LeetCode Sync] - Successful Pairs of Spells and Potions - Runtime - 680 ms (58.61%), Memory - 35.1 MB (85.28%)

---
+++ leetcode/2392-successful-pairs-of-spells-and-potions/solution.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def successfulPairs(self, spells, potions, success):
+        potions.sort()
+        m = len(potions)
+        result = []
+        
+        for spell in spells:
+            min_potion = (success + spell - 1) // spell
+            
+            left, right = 0, m
+            while left < right:
+                mid = (left + right) // 2
+                if potions[mid] >= min_potion:
+                    right = mid
+                else:
+                    left = mid + 1
+            
+            result.append(m - left)
+        
+        return result
