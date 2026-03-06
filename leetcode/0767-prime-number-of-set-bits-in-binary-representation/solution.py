af2377a03d253e960cbf5d4d8b4d0dcb4f49db02
AyushSingh360 <ayushsingh21109@gmail.com>
2026-03-06 02:54:50 +0000

    [LeetCode Sync] - Prime Number of Set Bits in Binary Representation - Runtime - 100 ms (71.70%), Memory - 12.6 MB (51.89%)

---
+++ leetcode/0767-prime-number-of-set-bits-in-binary-representation/solution.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def countPrimeSetBits(self, left, right):
+        """
+        :type left: int
+        :type right: int
+        :rtype: int
+        """
+        # Max bits needed for right <= 10^6 is < 21, so possible primes in that range:
+        prime_counts = {2, 3, 5, 7, 11, 13, 17, 19}
+        
+        ans = 0
+        for x in range(left, right + 1):
+            # Count set bits using builtin
+            cnt = bin(x).count("1")
+            if cnt in prime_counts:
+                ans += 1
+        return ans
+
