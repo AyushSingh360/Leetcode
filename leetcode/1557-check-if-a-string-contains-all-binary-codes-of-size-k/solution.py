35c1392cdf5e7ffa32961e9f04bbcce2c1f7891e
AyushSingh360 <ayushsingh21109@gmail.com>
2026-03-06 02:52:27 +0000

    [LeetCode Sync] - Check If a String Contains All Binary Codes of Size K - Runtime - 318 ms (47.24%), Memory - 63.2 MB (43.22%)

---
+++ leetcode/1557-check-if-a-string-contains-all-binary-codes-of-size-k/solution.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def hasAllCodes(self, s, k):
+        """
+        :type s: str
+        :type k: int
+        :rtype: bool
+        """
+        # If s is too short, impossible to contain all codes
+        if len(s) < k:
+            return False
+
+        seen = set()
+        # Slide window of size k and collect substrings
+        for i in range(len(s) - k + 1):
+            seen.add(s[i:i+k])
+
+        # There are 2^k possible binary codes of length k
+        return len(seen) == (1 << k)
+
