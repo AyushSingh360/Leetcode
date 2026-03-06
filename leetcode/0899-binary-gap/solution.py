84d133ecc225825215f54ebf342394d5b269d179
AyushSingh360 <ayushsingh21109@gmail.com>
2026-03-06 02:52:59 +0000

    [LeetCode Sync] - Binary Gap - Runtime - 0 ms (100.00%), Memory - 12.4 MB (55.00%)

---
+++ leetcode/0899-binary-gap/solution.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def binaryGap(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        b = bin(n)[2:]  # binary string without '0b'
+        prev = -1
+        ans = 0
+
+        for i, ch in enumerate(b):
+            if ch == '1':
+                if prev != -1:
+                    ans = max(ans, i - prev)
+                prev = i
+
+        return ans
+
