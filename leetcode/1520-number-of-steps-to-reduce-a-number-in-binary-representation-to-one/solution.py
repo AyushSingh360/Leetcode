99b46402c3d35915040f78ceed0652cf9f8037f8
AyushSingh360 <ayushsingh21109@gmail.com>
2026-03-06 02:48:56 +0000

    [LeetCode Sync] - Number of Steps to Reduce a Number in Binary Representation to One - Runtime - 3 ms (64.57%), Memory - 12.5 MB (3.14%)

---
+++ leetcode/1520-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/solution.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def numSteps(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        steps = 0
+        # Convert to list for easier manipulation
+        s = list(s)
+        
+        # Continue while s is not "1"
+        while len(s) > 1 or s[0] != '1':
+            if s[-1] == '0':  # Even number - divide by 2 (remove last 0)
+                s.pop()
+                steps += 1
+            else:  # Odd number - add 1
+                # Add 1 to binary string
+                i = len(s) - 1
+                while i >= 0 and s[i] == '1':
+                    s[i] = '0'
+                    i -= 1
+                
+                if i >= 0:
+                    s[i] = '1'
+                else:
+                    s.insert(0, '1')
+                
+                steps += 1
+        
+        return steps
