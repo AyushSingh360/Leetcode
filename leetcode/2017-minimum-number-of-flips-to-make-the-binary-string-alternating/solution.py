0781636e857d5c206efc6649857fa82ca4bac1be
AyushSingh360 <ayushsingh21109@gmail.com>
2026-03-07 10:11:41 +0000

    [LeetCode Sync] - Minimum Number of Flips to Make the Binary String Alternating - Runtime - 878 ms (12.98%), Memory - 25.7 MB (5.22%)

---
+++ leetcode/2017-minimum-number-of-flips-to-make-the-binary-string-alternating/solution.py
@@ -0,0 +1,46 @@
+class Solution(object):
+    def minFlips(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        n = len(s)
+        # Build two target strings of length 2n: "0101..." and "1010..."
+        t1 = []
+        t2 = []
+        for i in range(2 * n):
+            if i % 2 == 0:
+                t1.append('0')
+                t2.append('1')
+            else:
+                t1.append('1')
+                t2.append('0')
+        t1 = ''.join(t1)
+        t2 = ''.join(t2)
+
+        ss = s + s   # to simulate all rotations
+        diff1 = diff2 = 0
+        ans = n  # upper bound
+
+        left = 0
+        for right in range(2 * n):
+            # Expand window: compare ss[right] with both patterns
+            if ss[right] != t1[right]:
+                diff1 += 1
+            if ss[right] != t2[right]:
+                diff2 += 1
+
+            # When window length > n, shrink from left
+            if right - left + 1 > n:
+                if ss[left] != t1[left]:
+                    diff1 -= 1
+                if ss[left] != t2[left]:
+                    diff2 -= 1
+                left += 1
+
+            # When window length == n, this corresponds to some rotation
+            if right - left + 1 == n:
+                ans = min(ans, diff1, diff2)
+
+        return ans
+
