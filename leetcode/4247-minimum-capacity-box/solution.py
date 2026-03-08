b12e5d8c0a9b9fd54255f9c17d2211f41c7e7b61
AyushSingh360 <ayushsingh21109@gmail.com>
2026-03-08 02:31:45 +0000

    [LeetCode Sync] - Minimum Capacity Box - Runtime - 7 ms (4.75%), Memory - 12.3 MB (91.89%)

---
+++ leetcode/4247-minimum-capacity-box/solution.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def minimumIndex(self, capacity, itemSize):
+        ans = -1
+        best = 10**9  # larger than any possible capacity
+        
+        for i, c in enumerate(capacity):
+            if c >= itemSize and c < best:
+                best = c
+                ans = i
+        
+        return ans
+
