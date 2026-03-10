b5b4aa98e6a1c4fe0903091005a986811979fce7
AyushSingh360 <ayushsingh21109@gmail.com>
2026-03-10 04:22:15 +0000

    [LeetCode Sync] - Remove All Adjacent Duplicates in String II - Runtime - 47 ms (99.59%), Memory - 18.1 MB (83.47%)

---
+++ leetcode/1320-remove-all-adjacent-duplicates-in-string-ii/solution.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def removeDuplicates(self, s, k):
+        """
+        :type s: str
+        :type k: int
+        :rtype: str
+        """
+        # Stack stores (character, count) pairs
+        stack = []
+        
+        for char in s:
+            if stack and stack[-1][0] == char:
+                # Same character as top of stack, increment count
+                stack[-1][1] += 1
+                # If count reaches k, remove this group
+                if stack[-1][1] == k:
+                    stack.pop()
+            else:
+                # Different character, push new entry
+                stack.append([char, 1])
+        
+        # Build result from stack
+        result = []
+        for char, count in stack:
+            result.append(char * count)
+        
+        return ''.join(result)
