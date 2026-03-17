b940a808b197674e2de521238dd1114130a1511e
AyushSingh360 <ayushsingh21109@gmail.com>
2026-03-17 14:56:28 +0000

    [LeetCode Sync] - Largest Submatrix With Rearrangements - Runtime - 117 ms (88.72%), Memory - 33.8 MB (76.68%)

---
+++ leetcode/1845-largest-submatrix-with-rearrangements/solution.py
@@ -0,0 +1,20 @@
+class Solution:
+    def largestSubmatrix(self, matrix: List[List[int]]) -> int:
+        m, n = len(matrix), len(matrix[0])
+        heights = [0] * n
+        max_area = 0
+        
+        for row in matrix:
+            for j in range(n):
+                if row[j] == 1:
+                    heights[j] += 1
+                else:
+                    heights[j] = 0
+            
+            sorted_heights = sorted(heights, reverse=True)
+            for j in range(n):
+                area = sorted_heights[j] * (j + 1)
+                if area > max_area:
+                    max_area = area
+        
+        return max_area
