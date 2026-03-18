9136e662457d8676ff27f3fbd1326da5dece43c8
AyushSingh360 <ayushsingh21109@gmail.com>
2026-03-18 12:44:49 +0000

    [LeetCode Sync] - Count Submatrices with Top-Left Element and Sum Less Than k - Runtime - 212 ms (73.75%), Memory - 66.8 MB (12.01%)

---
+++ leetcode/3338-count-submatrices-with-top-left-element-and-sum-less-than-k/solution.py
@@ -0,0 +1,24 @@
+from typing import List
+
+class Solution:
+    def countSubmatrices(self, grid: List[List[int]], k: int) -> int:
+        m, n = len(grid), len(grid[0])
+
+        # pref[i][j] = sum of rectangle (0,0) to (i-1, j-1)
+        pref = [[0] * (n + 1) for _ in range(m + 1)]
+        for i in range(m):
+            row_sum = 0
+            for j in range(n):
+                row_sum += grid[i][j]
+                pref[i + 1][j + 1] = pref[i][j + 1] + row_sum
+
+        ans = 0
+        # For each row, find how many prefix sums <= k from the left
+        for i in range(1, m + 1):
+            j = 1
+            while j <= n and pref[i][j] <= k:
+                j += 1
+            ans += (j - 1)
+
+        return ans
+
