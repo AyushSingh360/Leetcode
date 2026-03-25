eb043f04eb6652d2508af667c5e54309331b06af
AyushSingh360 <ayushsingh21109@gmail.com>
2026-03-25 03:57:49 +0000

    [LeetCode Sync] - Construct Product Matrix - Runtime - 155 ms (21.46%), Memory - 49.5 MB (10.69%)

---
+++ leetcode/3031-construct-product-matrix/solution.py
@@ -0,0 +1,36 @@
+from typing import List
+
+class Solution:
+    def constructProductMatrix(self, grid: List[List[int]]) -> List[List[int]]:
+        MOD = 12345
+        n, m = len(grid), len(grid[0])
+        k = n * m
+
+        a = [0] * k
+        idx = 0
+        for i in range(n):
+            for j in range(m):
+                a[idx] = grid[i][j] % MOD
+                idx += 1
+
+        pref = [1] * (k + 1)
+        for i in range(k):
+            pref[i + 1] = (pref[i] * a[i]) % MOD
+
+        suff = [1] * (k + 1)
+        for i in range(k - 1, -1, -1):
+            suff[i] = (suff[i + 1] * a[i]) % MOD
+
+        res_flat = [0] * k
+        for i in range(k):
+            res_flat[i] = (pref[i] * suff[i + 1]) % MOD
+
+        ans = [[0] * m for _ in range(n)]
+        idx = 0
+        for i in range(n):
+            for j in range(m):
+                ans[i][j] = res_flat[idx]
+                idx += 1
+
+        return ans
+
