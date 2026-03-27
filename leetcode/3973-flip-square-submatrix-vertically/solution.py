cb6620c3189a7b6c50c4b24da69f7a3999cf82c5
AyushSingh360 <ayushsingh21109@gmail.com>
2026-03-27 14:25:26 +0000

    [LeetCode Sync] - Flip Square Submatrix Vertically - Runtime - 4 ms (7.72%), Memory - 19.3 MB (99.07%)

---
+++ leetcode/3973-flip-square-submatrix-vertically/solution.py
@@ -0,0 +1,17 @@
+from typing import List
+
+class Solution:
+    def reverseSubmatrix(self, grid: List[List[int]], x: int, y: int, k: int) -> List[List[int]]:
+        # top and bottom row indices of the k x k square
+        top = x
+        bottom = x + k - 1
+
+        # swap rows within the submatrix vertically
+        while top < bottom:
+            # swap only the columns inside the submatrix: y to y + k - 1
+            for col in range(y, y + k):
+                grid[top][col], grid[bottom][col] = grid[bottom][col], grid[top][col]
+            top += 1
+            bottom -= 1
+
+        return grid
