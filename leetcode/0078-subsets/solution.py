9f5321fb5dd8a851a3bd70fd35fb0b71657f7708
AyushSingh360 <ayushsingh21109@gmail.com>
2026-03-27 02:58:00 +0000

    [LeetCode Sync] - Subsets - Runtime - 0 ms (100.00%), Memory - 19.5 MB (28.53%)

---
+++ leetcode/0078-subsets/solution.py
@@ -0,0 +1,25 @@
+from typing import List
+
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        subset = []
+
+        def backtrack(i: int) -> None:
+            # If we've considered all elements, record the current subset
+            if i == len(nums):
+                res.append(subset.copy())
+                return
+
+            # Choice 1: do not include nums[i]
+            backtrack(i + 1)
+
+            # Choice 2: include nums[i]
+            subset.append(nums[i])
+            backtrack(i + 1)
+
+            # Backtrack
+            subset.pop()
+
+        backtrack(0)
+        return res
