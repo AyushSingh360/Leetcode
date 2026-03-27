fa70f90c4845659f3a021d704c93f3098072cb23
AyushSingh360 <ayushsingh21109@gmail.com>
2026-03-27 02:46:12 +0000

    [LeetCode Sync] - Combinations - Runtime - 103 ms (79.18%), Memory - 61.6 MB (10.39%)

---
+++ leetcode/0077-combinations/solution.py
@@ -0,0 +1,20 @@
+from typing import List
+
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        res = []
+
+        def backtrack(start: int, comb: List[int]) -> None:
+            # If the combination is complete, save it
+            if len(comb) == k:
+                res.append(comb.copy())
+                return
+
+            # Standard combinations backtracking
+            for i in range(start, n + 1):
+                comb.append(i)
+                backtrack(i + 1, comb)
+                comb.pop()
+
+        backtrack(1, [])
+        return res
