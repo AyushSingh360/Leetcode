7417949085a0d2b4ab28b4247b34a0195858ad8c
AyushSingh360 <ayushsingh21109@gmail.com>
2026-03-29 03:37:18 +0000

    [LeetCode Sync] - Remove Duplicates from Sorted Array II - Runtime - 92 ms (31.10%), Memory - 21.7 MB (93.47%)

---
+++ leetcode/0080-remove-duplicates-from-sorted-array-ii/solution.py
@@ -0,0 +1,15 @@
+from typing import List
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        # index where we will write the next allowed element
+        k = 0
+
+        for x in nums:
+            # Always keep first two elements, after that only keep if
+            # current x is not the same as the element two positions back
+            if k < 2 or x != nums[k - 2]:
+                nums[k] = x
+                k += 1
+
+        return k
