e85b62c0c06616747889d514e2e583d12af5fdd9
AyushSingh360 <ayushsingh21109@gmail.com>
2026-03-30 03:13:04 +0000

    [LeetCode Sync] - Pascal's Triangle - Runtime - 0 ms (100.00%), Memory - 19.3 MB (28.87%)

--- leetcode/0118-pascals-triangle/solution.py
+++ leetcode/0118-pascals-triangle/solution.py
@@ -1,16 +1,13 @@
-class Solution(object):
-    def generate(self, numRows):
-        """
-        :type numRows: int
-        :rtype: List[List[int]]
-        """
-        triangle = []
+from typing import List
 
-        for i in range(numRows):
-            row = [1] * (i + 1)
-            for j in range(1, i):
-                row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        triangle = [[1]]
+        for _ in range(numRows - 1):
+            prev = triangle[-1]
+            row = [1]
+            for j in range(len(prev) - 1):
+                row.append(prev[j] + prev[j + 1])
+            row.append(1)
             triangle.append(row)
-
         return triangle
-
