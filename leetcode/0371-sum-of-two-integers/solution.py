df697e4513a6100967bd017c42f0b3e5da61ce24
AyushSingh360 <ayushsingh21109@gmail.com>
2026-03-30 03:20:08 +0000

    [LeetCode Sync] - Sum of Two Integers - Runtime - 0 ms (100.00%), Memory - 19.3 MB (53.67%)

---
+++ leetcode/0371-sum-of-two-integers/solution.py
@@ -0,0 +1,16 @@
+class Solution:
+    def getSum(self, a: int, b: int) -> int:
+        MASK = 0xFFFFFFFF       # 32 bits
+        MAX_INT = 0x7FFFFFFF    # max positive 32-bit int
+
+        # convert to 32-bit representation
+        a &= MASK
+        b &= MASK
+
+        while b != 0:
+            carry = (a & b) << 1
+            a = (a ^ b) & MASK
+            b = carry & MASK
+
+        # if a is negative in 32-bit, convert from two's complement
+        return a if a <= MAX_INT else ~(a ^ MASK)
