7422cf229107b9e34c766e1595efa298c7ed4962
AyushSingh360 <ayushsingh21109@gmail.com>
2026-03-30 03:22:17 +0000

    [LeetCode Sync] - Number of Digit One - Runtime - 0 ms (100.00%), Memory - 19.4 MB (10.70%)

---
+++ leetcode/0233-number-of-digit-one/solution.py
@@ -0,0 +1,23 @@
+class Solution:
+    def countDigitOne(self, n: int) -> int:
+        if n <= 0:
+            return 0
+
+        count = 0
+        base = 1  # 1, 10, 100, ...
+
+        while base <= n:
+            high = n // (base * 10)
+            cur  = (n // base) % 10
+            low  = n % base
+
+            if cur == 0:
+                count += high * base
+            elif cur == 1:
+                count += high * base + (low + 1)
+            else:  # cur >= 2
+                count += (high + 1) * base
+
+            base *= 10
+
+        return count
