89a445b3e64adabf459a74e00a7255e9bc1491df
AyushSingh360 <ayushsingh21109@gmail.com>
2026-04-01 14:39:37 +0000

    [LeetCode Sync] - Robot Collisions - Runtime - 99 ms (66.81%), Memory - 42.5 MB (46.96%)

---
+++ leetcode/2846-robot-collisions/solution.py
@@ -0,0 +1,30 @@
+from typing import List
+
+class Solution:
+    def survivedRobotsHealths(self, positions: List[int], healths: List[int], directions: str) -> List[int]:
+        n = len(positions)
+        robots = sorted([(positions[i], i) for i in range(n)])
+        
+        stack = []          # indices of surviving right-moving robots
+        alive = [True] * n
+        
+        for _, i in robots:
+            if directions[i] == 'R':
+                stack.append(i)
+            else:
+                while stack and alive[i]:
+                    j = stack[-1]   # right-moving robot
+
+                    if healths[j] < healths[i]:
+                        alive[j] = False
+                        stack.pop()
+                        healths[i] -= 1
+                    elif healths[j] > healths[i]:
+                        alive[i] = False
+                        healths[j] -= 1
+                    else:
+                        alive[i] = False
+                        alive[j] = False
+                        stack.pop()
+        
+        return [healths[i] for i in range(n) if alive[i]]
