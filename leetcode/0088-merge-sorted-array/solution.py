b0d6e6371db7c8f7be8b2bbf28759d0157449da1
AyushSingh360 <ayushsingh21109@gmail.com>
2026-04-02 13:50:20 +0000

    [LeetCode Sync] - Merge Sorted Array - Runtime - 0 ms (100.00%), Memory - 19.3 MB (39.21%)

---
+++ leetcode/0088-merge-sorted-array/solution.py
@@ -0,0 +1,20 @@
+from typing import List
+
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        i = m - 1          # last valid index in nums1
+        j = n - 1          # last index in nums2
+        k = m + n - 1      # last position in nums1 (total length)
+
+        # Merge from the back so we do not overwrite elements in nums1
+        while j >= 0:
+            if i >= 0 and nums1[i] > nums2[j]:
+                nums1[k] = nums1[i]
+                i -= 1
+            else:
+                nums1[k] = nums2[j]
+                j -= 1
+            k -= 1
