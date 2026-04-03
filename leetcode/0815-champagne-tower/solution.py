98754b7db9f53a3436b1989be316a8e6f3386ead
AyushSingh360 <ayushsingh21109@gmail.com>
2026-04-03 01:58:45 +0000

    [LeetCode Sync] - Champagne Tower - Runtime - 65 ms (97.14%), Memory - 19.4 MB (66.94%)

---
+++ leetcode/0815-champagne-tower/solution.py
@@ -0,0 +1,16 @@
+from typing import List
+
+class Solution:
+    def champagneTower(self, poured: int, query_row: int, query_glass: int) -> float:
+        # dp[row][glass] = amount of champagne in that glass (can be > 1 before split)[web:39]
+        dp = [[0.0] * (i + 1) for i in range(query_row + 2)]
+        dp[0][0] = float(poured)
+
+        for row in range(query_row):
+            for glass in range(row + 1):
+                if dp[row][glass] > 1.0:
+                    excess = (dp[row][glass] - 1.0) / 2.0
+                    dp[row + 1][glass] += excess
+                    dp[row + 1][glass + 1] += excess
+
+        return min(1.0, dp[query_row][query_glass])
