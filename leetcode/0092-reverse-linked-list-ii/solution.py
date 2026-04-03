9abcf2d7d121bf418c84fba60a851b4080091702
AyushSingh360 <ayushsingh21109@gmail.com>
2026-04-03 10:34:09 +0000

    [LeetCode Sync] - Reverse Linked List II - Runtime - 0 ms (100.00%), Memory - 19.2 MB (98.73%)

---
+++ leetcode/0092-reverse-linked-list-ii/solution.py
@@ -0,0 +1,31 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+from typing import Optional
+
+class Solution:
+    def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
+        if not head or left == right:
+            return head
+
+        dummy = ListNode(0)
+        dummy.next = head
+
+        # move prev to node just before 'left'
+        prev = dummy
+        for _ in range(left - 1):
+            prev = prev.next
+
+        curr = prev.next
+
+        # reverse [left, right] using head insertion
+        for _ in range(right - left):
+            temp = curr.next
+            curr.next = temp.next
+            temp.next = prev.next
+            prev.next = temp
+
+        return dummy.next
