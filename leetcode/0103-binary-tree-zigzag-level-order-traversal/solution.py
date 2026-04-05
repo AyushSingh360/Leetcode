4ec0051ad8ae6f725457d73864d784213fe9004f
AyushSingh360 <ayushsingh21109@gmail.com>
2026-04-05 13:45:47 +0000

    [LeetCode Sync] - Binary Tree Zigzag Level Order Traversal - Runtime - 0 ms (100.00%), Memory - 19.4 MB (83.42%)

---
+++ leetcode/0103-binary-tree-zigzag-level-order-traversal/solution.py
@@ -0,0 +1,38 @@
+from collections import deque
+from typing import Optional, List
+
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if not root:
+            return []
+
+        q = deque([root])
+        left_to_right = True
+        ans = []
+
+        while q:
+            level_size = len(q)
+            level = []
+
+            for _ in range(level_size):
+                node = q.popleft()
+                level.append(node.val)
+
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+
+            if not left_to_right:
+                level.reverse()
+
+            ans.append(level)
+            left_to_right = not left_to_right
+
+        return ans
