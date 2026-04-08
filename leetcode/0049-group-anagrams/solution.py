ec9e1ba6127428d8b347166f6a3e24544b668bad
AyushSingh360 <ayushsingh21109@gmail.com>
2026-04-08 16:01:22 +0000

    [LeetCode Sync] - Group Anagrams - Runtime - 12 ms (58.17%), Memory - 21.9 MB (88.32%)

--- leetcode/0049-group-anagrams/solution.py
+++ leetcode/0049-group-anagrams/solution.py
@@ -1,16 +1,12 @@
+from typing import List
 from collections import defaultdict
 
-class Solution(object):
-    def groupAnagrams(self, strs):
-        """
-        :type strs: List[str]
-        :rtype: List[List[str]]
-        """
-        anagrams = defaultdict(list)
-        
-        for word in strs:
-            key = ''.join(sorted(word))  # sort characters to form a signature
-            anagrams[key].append(word)
-        
-        return list(anagrams.values())
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        groups = defaultdict(list)
 
+        for word in strs:
+            key = ''.join(sorted(word))   # canonical form
+            groups[key].append(word)
+
+        return list(groups.values())
