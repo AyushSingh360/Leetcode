65218e02c5d8f71963a2585ab13a6bdd39bcdb21
AyushSingh360 <ayushsingh21109@gmail.com>
2026-04-09 02:55:56 +0000

    [LeetCode Sync] - Check If All 1's Are at Least Length K Places Away - Runtime - 11 ms (52.73%), Memory - 23.5 MB (50.43%)

---
+++ leetcode/1548-check-if-all-1s-are-at-least-length-k-places-away/solution.py
@@ -0,0 +1,13 @@
+from typing import List
+
+class Solution:
+    def kLengthApart(self, nums: List[int], k: int) -> bool:
+        prev = -10**9  # effectively -infinity
+
+        for i, x in enumerate(nums):
+            if x == 1:
+                if i - prev - 1 < k:
+                    return False
+                prev = i
+
+        return True
