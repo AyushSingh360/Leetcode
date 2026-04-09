8f345d5e143f8b178b3d9d23f117cf095a78b4c7
AyushSingh360 <ayushsingh21109@gmail.com>
2026-04-09 03:04:39 +0000

    [LeetCode Sync] - Next Greater Numerically Balanced Number - Runtime - 1989 ms (53.60%), Memory - 19.2 MB (75.74%)

---
+++ leetcode/2174-next-greater-numerically-balanced-number/solution.py
@@ -0,0 +1,22 @@
+from typing import List
+
+class Solution:
+    def nextBeautifulNumber(self, n: int) -> int:
+        def is_balanced(x: int) -> bool:
+            cnt = [0] * 10
+            t = x
+            while t > 0:
+                t, d = divmod(t, 10)
+                cnt[d] += 1
+            # A number is numerically balanced if for every digit d used,
+            # its count equals d.
+            for d in range(10):
+                if cnt[d] != 0 and cnt[d] != d:
+                    return False
+            return True
+
+        x = n + 1
+        while True:
+            if is_balanced(x):
+                return x
+            x += 1
