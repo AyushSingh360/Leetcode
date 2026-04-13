51a3f5d1e2dda0ccb5a798feefdec2157a353049
AyushSingh360 <ayushsingh21109@gmail.com>
2026-04-13 02:36:18 +0000

    [LeetCode Sync] - Minimum Distance to the Target Element - Runtime - 4 ms (5.58%), Memory - 19.4 MB (49.12%)

---
+++ leetcode/1975-minimum-distance-to-the-target-element/solution.py
@@ -0,0 +1,7 @@
+class Solution:
+    def getMinDistance(self, nums: List[int], target: int, start: int) -> int:
+        ans = float('inf')
+        for i, val in enumerate(nums):
+            if val == target:
+                ans = min(ans, abs(i - start))
+        return ans
