051d825dcb287a9f0679b38b8f3906f1f24d6fcd
AyushSingh360 <ayushsingh21109@gmail.com>
2026-04-15 02:44:43 +0000

    [LeetCode Sync] - Set Intersection Size At Least Two - Runtime - 7 ms (78.36%), Memory - 20.7 MB (25.28%)

---
+++ leetcode/0759-set-intersection-size-at-least-two/solution.py
@@ -0,0 +1,26 @@
+class Solution:
+    def intersectionSizeTwo(self, intervals: List[List[int]]) -> int:
+        # Sort by end asc; if same end, by start desc
+        intervals.sort(key=lambda x: (x[1], -x[0]))
+        
+        # last2 < last are the two largest points chosen so far
+        last2 = -1
+        last = -1
+        res = 0
+        
+        for s, e in intervals:
+            if s <= last2:
+                # interval already has at least two points
+                continue
+            elif s > last:
+                # interval has none of {last2, last}: add two new points
+                res += 2
+                last2 = e - 1
+                last = e
+            else:
+                # interval has only `last`: add one more point
+                res += 1
+                last2 = last
+                last = e
+        
+        return res
