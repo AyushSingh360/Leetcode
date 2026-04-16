ccc3438d46d463cb1720b195f703ae8aeb496acf
AyushSingh360 <ayushsingh21109@gmail.com>
2026-04-16 01:34:22 +0000

    [LeetCode Sync] - Count Partitions with Even Sum Difference - Runtime - 0 ms (100.00%), Memory - 19.1 MB (88.33%)

---
+++ leetcode/3704-count-partitions-with-even-sum-difference/solution.py
@@ -0,0 +1,9 @@
+from typing import List
+
+class Solution:
+    def countPartitions(self, nums: List[int]) -> int:
+        total = sum(nums)
+        n = len(nums)
+        if total % 2 != 0:
+            return 0
+        return n - 1
