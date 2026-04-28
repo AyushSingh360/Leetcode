6347d27eeead5dc093963d00fac010fa6ab0ac6a
AyushSingh360 <ayushsingh21109@gmail.com>
2026-04-28 01:50:43 +0000

    [LeetCode Sync] - Kth Smallest Element in a BST - Runtime - 0 ms (100.00%), Memory - 22.1 MB (87.50%)

---
+++ leetcode/0230-kth-smallest-element-in-a-bst/solution.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+from typing import Optional
+
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        stack = []
+        cur = root
+
+        while cur or stack:
+            # go as left as possible
+            while cur:
+                stack.append(cur)
+                cur = cur.left
+
+            # process the next smallest node
+            cur = stack.pop()
+            k -= 1
+            if k == 0:
+                return cur.val
+
+            # go to right subtree
+            cur = cur.right
