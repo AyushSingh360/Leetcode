daa53f208a46f0b12f9b6bd7e0a4976a36d6dc8c
AyushSingh360 <ayushsingh21109@gmail.com>
2026-04-28 01:48:47 +0000

    [LeetCode Sync] - Basic Calculator II - Runtime - 55 ms (70.70%), Memory - 22.3 MB (40.54%)

---
+++ leetcode/0227-basic-calculator-ii/solution.py
@@ -0,0 +1,27 @@
+class Solution:
+    def calculate(self, s: str) -> int:
+        stack = []
+        num = 0
+        sign = '+'  # previous operator
+
+        for i, c in enumerate(s):
+            if c.isdigit():
+                num = num * 10 + int(c)
+
+            # if c is an operator or we're at the end, process the previous sign
+            if c in '+-*/' or i == len(s) - 1:
+                if sign == '+':
+                    stack.append(num)
+                elif sign == '-':
+                    stack.append(-num)
+                elif sign == '*':
+                    stack.append(stack.pop() * num)
+                elif sign == '/':
+                    prev = stack.pop()
+                    # truncate toward zero
+                    stack.append(int(prev / num))
+
+                sign = c
+                num = 0
+
+        return sum(stack)
