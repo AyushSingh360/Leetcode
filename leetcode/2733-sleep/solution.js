dac17254e86171272d7229509fd83971ccbf4ae7
AyushSingh360 <ayushsingh21109@gmail.com>
2026-05-01 02:50:18 +0000

    [LeetCode Sync] - Sleep - Runtime - 44 ms (58.47%), Memory - 52.5 MB (94.25%)

---
+++ leetcode/2733-sleep/solution.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} millis
+ * @return {Promise}
+ */
+async function sleep(millis) {
+    return new Promise(resolve => setTimeout(resolve, millis));  // [web:66][web:67]
+}
+
+
+/** 
+ * let t = Date.now()
+ * sleep(100).then(() => console.log(Date.now() - t)) // ~100
+ */
