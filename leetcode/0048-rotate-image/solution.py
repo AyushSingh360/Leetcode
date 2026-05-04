d9de74cd4ed4c285030baf3a358fea209ac2bc5f
AyushSingh360 <ayushsingh21109@gmail.com>
2026-05-04 01:50:49 +0000

    [LeetCode Sync] - Rotate Image - Runtime - 3 ms (3.65%), Memory - 19.3 MB (28.55%)

--- leetcode/0048-rotate-image/solution.py
+++ leetcode/0048-rotate-image/solution.py
@@ -1,16 +1,15 @@
-class Solution(object):
-    def rotate(self, matrix):
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
         """
-        :type matrix: List[List[int]]
-        :rtype: None. Do not return anything, modify matrix in-place instead.
+        Do not return anything, modify matrix in-place instead.
         """
         n = len(matrix)
-
-        # Step 1: Transpose (swap matrix[i][j] with matrix[j][i])
+        
+        # 1. Transpose the matrix (swap matrix[i][j] with matrix[j][i] for i < j)
         for i in range(n):
             for j in range(i + 1, n):
                 matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
-
-        # Step 2: Reverse each row
-        for row in matrix:
-            row.reverse()
+        
+        # 2. Reverse each row
+        for i in range(n):
+            matrix[i].reverse()
