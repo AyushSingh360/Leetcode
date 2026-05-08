09f0f3aa39ed2370155a5ed4bb128209ae9b86e2
AyushSingh360 <ayushsingh21109@gmail.com>
2026-05-08 14:26:56 +0000

    [LeetCode Sync] - Perfect Squares - Runtime - 1327 ms (77.86%), Memory - 19.4 MB (58.99%)

---
+++ leetcode/0279-perfect-squares/solution.py
@@ -0,0 +1,19 @@
+class Solution:
+    def numSquares(self, n: int) -> int:
+        # dp[i] = least number of perfect squares that sum to i
+        dp = [0] + [float('inf')] * n
+
+        # precompute all perfect squares <= n
+        squares = []
+        k = 1
+        while k * k <= n:
+            squares.append(k * k)
+            k += 1
+
+        for i in range(1, n + 1):
+            for sq in squares:
+                if sq > i:
+                    break
+                dp[i] = min(dp[i], dp[i - sq] + 1)
+
+        return dp[n]
