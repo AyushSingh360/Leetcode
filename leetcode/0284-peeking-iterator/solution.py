7cf4633534677b62be1e61f7190b5d3e58881969
AyushSingh360 <ayushsingh21109@gmail.com>
2026-05-10 01:55:11 +0000

    [LeetCode Sync] - Peeking Iterator - Runtime - 52 ms (20.53%), Memory - 19.5 MB (56.10%)

---
+++ leetcode/0284-peeking-iterator/solution.py
@@ -0,0 +1,52 @@
+# Below is the interface for Iterator, which is already defined for you.
+#
+# class Iterator:
+#     def __init__(self, nums):
+#         """
+#         Initializes an iterator object to the beginning of a list.
+#         :type nums: List[int]
+#         """
+#
+#     def hasNext(self):
+#         """
+#         Returns true if the iteration has more elements.
+#         :rtype: bool
+#         """
+#
+#     def next(self):
+#         """
+#         Returns the next element in the iteration.
+#         :rtype: int
+#         """
+
+class PeekingIterator:
+    def __init__(self, iterator):
+        """
+        Initialize your data structure here.
+        :type iterator: Iterator
+        """
+        self.iter = iterator
+        # Preload the first element if it exists
+        self._next = self.iter.next() if self.iter.hasNext() else None
+
+    def peek(self):
+        """
+        Returns the next element in the iteration without advancing the iterator.
+        :rtype: int
+        """
+        return self._next
+
+    def next(self):
+        """
+        :rtype: int
+        """
+        # Return the cached value, then advance and refresh the cache
+        curr = self._next
+        self._next = self.iter.next() if self.iter.hasNext() else None
+        return curr
+
+    def hasNext(self):
+        """
+        :rtype: bool
+        """
+        return self._next is not None
